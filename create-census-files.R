f39b07ebdecbe2b5df6e4ece007d6b9562479615
Mary Shelley <mshelley@rstudio03.research.sesync.org>
2017-12-14 11:55:00 -0500

    created two digit naics code and separate data set of naics cat descriptions

--- create-census-files.R
+++ create-census-files.R
@@ -5,7 +5,50 @@
 library(magrittr)
 library(dplyr)
 
+# Select only identifiers and number of establishments
 cbp15 <- read.csv('~/censusdat/cbp15co.txt') %>%
          select(FIPSTATE, FIPSCTY, NAICS, EST)
 
-test<- filter(cbp15, substr(NAICS, 3, 4)=='----')
+# Limit to two-digit NAICS and create new var with that two digits 
+# cbp15_2 has one record per county per 2-digit NAICS, where EST for the code !=0
+cbp15_2 <- filter(cbp15, substr(NAICS, 3, 6)=='----') %>%
+       mutate(naics = as.numeric((ifelse(substr(NAICS,1,2) != '--', substr(NAICS,1,2), 0)))) %>%
+       select(-NAICS)
+  
+# TODO: Make a new variable that combines state and county fips by combining and padding the
+# two separate vars
+
+
+# Create data set of NAICS codes
+naics <- c(11,21,22,23,31,42,44,48,51,52,53,54,55,56,61,62,71,72,81,92,0)
+naicsTxt <- c('Agriculture, Forestry, Fishing and Hunting',
+       'Mining',
+       'Utilities',
+       'Construction',
+       'Manufacturing',
+       'Wholesale Trade',
+       'Retail Trade',
+       'Transportation and Warehousing',
+       'Information',
+       'Finance and Insurance',
+       'Real Estate Rental and Leasing',
+       'Professional, Scientific, and Technical Services',
+       'Management of Companies and Enterprises',
+       'Administrative and Support and Waste Management and Remediation Services',
+       'Educational Services',
+       'Health Care and Social Assistance',
+       'Arts, Entertainment, and Recreation',
+       'Accommodation and Food Services',
+       'Other Services (except Public Administration)',
+       'Public Administration',
+       'TOTAL')
+naicsDat<-data.frame(cbind(naics,naicsTxt))
+
+# TODO: decide whether to make naics code numeric to match cbp15_2
+
+
+# TODO: Create data set of county names to merge 
+
+
+
+
